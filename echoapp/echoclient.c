0e6c9e75efa87d74a3d238b843b45b9e49e1020d
Arne Herdick <sujomear@arcor.de>
2016-04-20 17:54:27 +0200

    can now establish connection

--- echoapp/echoclient.c
+++ echoapp/echoclient.c
@@ -1,8 +1,38 @@
 /* echoclient.c */
 
 #include <cnaiapi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
+  if(argc != 3){
+    printf("usage: echoclient <computername> <appnum>\n");
+    return 0;
+  }
+
+  // get server name
+  computer serverComputer = 
+      cname_to_comp(argv[1]);
+
+  if(serverComputer == -1){
+    printf("Bad host entered, try 127.0.0.1");
+    return 0;
+  }
+
+  // get application id
+  appnum applicationIdentifier = 
+      atoi(argv[2]);
+
+  //try to contact the server
+  connection serverConnection = 
+      make_contact(serverComputer, applicationIdentifier);
+
+  if(serverConnection == -1)
+    printf("no connection accomplished\n");
+  else
+    printf("connection established, id: %i\n", serverConnection);
+
+  
 	return 0;
 }
--- echoapp/echoserver.c
+++ echoapp/echoserver.c
@@ -1,8 +1,35 @@
 /* echoserver.c */
 
 #include <cnaiapi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
+  if(argc != 2){
+    printf("usage: echoserver <appnum>\n");
+    return 0;
+  }
+
+  // get application id
+  appnum applicationIdentifier = 
+      atoi(argv[1]);
+
+  int socket = 
+      get_server_socket(applicationIdentifier);
+
+  printf("Using socket %i\n", socket);
+
+  printf("Waiting for connection...\n");
+
+  // wait for connection
+  connection clientConnection = 
+      await_contact(applicationIdentifier);
+
+  //react
+  if(clientConnection == -1)
+    printf("no connection established\n");
+  else
+    printf("connection established, connection id: %i\n", clientConnection);
 	return 0;
 }
