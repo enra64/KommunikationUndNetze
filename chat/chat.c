054004b743e87e9dc511275356eb609ab7ccb18b
enra64 <sujomear@arcor.de>
2016-04-28 16:55:49 +0200

    catch sigint to end connection

--- chat/chat.c
+++ chat/chat.c
@@ -1,13 +1,15 @@
-/* echoclient.c */
-
 #include <cnaiapi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <poll.h>
+#include <signal.h>
+#include <unistd.h>
 
 // buffer for everything
 char buffer[1024];
+// network file descriptor
+connection network;
 
 // if this function is called without input, it blocks
 // until input is received
@@ -24,8 +26,19 @@ void echo(int fdIn, int fdOut){
   write(fdOut, buffer, receiveLength);
 }
 
+// handle ^C by ending contact
+void sig_handler(int signal){
+  if(signal == SIGINT){
+    printf("\nReceived SIGINT, closing connection\n");
+    end_contact(network);
+    exit(0);
+  }
+  else
+    printf("Unknown signal %i received", signal);
+}
+
 // blocking function
-void echoing(connection network){
+void echoing(){
   // polling structs
   struct pollfd pollStructs[2] = {
     {0, POLLIN, 0}, // poll stdin
@@ -56,14 +69,12 @@ int main(int argc, char *argv[])
   computer serverComputer;
   // application number - this directly translates to port.
   appnum applicationIdentifier;
-  // network file descriptor
-  connection network;
 
   // get server name
   serverComputer = cname_to_comp(argv[1]);
 
   if(serverComputer == -1){
-    printf("Bad host entered, try 127.0.0.1\n");
+    printf("Bad host entered, try localhost\n");
     return 0;
   }
 
@@ -77,22 +88,26 @@ int main(int argc, char *argv[])
 
   // check connection validity
   if(network == -1){
-    printf("could not connect, starting server\n");
+    printf("Could not connect, starting server\n");
     // wait for connection
     network = await_contact(applicationIdentifier);
     
     //react
     if(network == -1)
-      printf("no connection established\n");
+      printf("No client found within timeout\n");
     else
-      printf("connection established, connection id: %i\n", network);
+      printf("Client connected, id: %i\n", network);
   }
   else{
-    printf("connection established, id: %i\n", network);
+    printf("Server found, id: %i\n", network);
   }
+  
+  // handle ^C
+  if (signal(SIGINT, sig_handler) == SIG_ERR)
+    printf("\ncan't catch SIGINT\n");
   
   // just echo from stdin to network and vice versa if a connection exists
   if(network != -1)
-    echoing(network);
+    echoing();
   return 0;
 }
