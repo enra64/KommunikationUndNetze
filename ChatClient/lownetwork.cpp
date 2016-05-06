5e1eb43a182d32fb84f48c3efa0585ee7f966587
enra64 <sujomear@arcor.de>
2016-05-06 19:59:14 +0200

    added most of the socket implementation

--- ChatClient/lownetwork.cpp
+++ ChatClient/lownetwork.cpp
@@ -1,6 +1,64 @@
 #include "lownetwork.h"
 
-bool LowNetwork::getHostAddress(QString hostName, unsigned long& address){
+int LowNetwork::server(const QString port){
+    bool validPort;
+    short shortport = port.toShort(&validPort);
+    if(!validPort)
+        return -1;
+
+    mAdress = htonl(INADDR_ANY);
+
+    struct sockaddr_in serverStruct, clientStruct;
+    mSocketHandle = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(mSocketHandle < 0)
+        return -3;
+
+    serverStruct.sin_family = AF_INET;
+    serverStruct.sin_port = htons(shortport);
+    serverStruct.sin_addr.s_addr = mAdress;
+
+    if(bind(mSocketHandle, (struct sockaddr *) &serverStruct, sizeof(serverStruct)) < 0)
+        return -4;
+
+    if(listen(mSocketHandle, 4) < 0)
+        return -5;
+
+    int clientSocketHandle;
+
+    unsigned int clientLength = sizeof(clientStruct);
+    if((clientSocketHandle = accept(mSocketHandle, (struct sockaddr *) &clientStruct, &clientLength)) < 0)
+        return -6;
+
+    // successfully navigated the mine field
+    return 0;
+}
+
+int LowNetwork::client(const QString host, const QString port){
+    bool validPort;
+    short shortport = port.toShort(&validPort);
+
+    if(!validPort)
+        return -1;
+
+    if(!getHostAddress(host, mAdress))
+        return -2;
+
+    struct sockaddr_in peerDescription;
+    mSocketHandle = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(mSocketHandle < 0)
+        return -3;
+
+    peerDescription.sin_family = AF_INET;
+    peerDescription.sin_port = htons(shortport);
+    peerDescription.sin_addr.s_addr = mAdress;
+
+    if(connect(mSocketHandle, (struct sockaddr *) &peerDescription, sizeof(peerDescription)) < 0)
+        return -4;
+
+    return 0;
+}
+
+bool LowNetwork::getHostAddress(const QString hostName, unsigned long& address){
     struct hostent * host;
     struct in_addr h_addr;
 
@@ -10,17 +68,6 @@ bool LowNetwork::getHostAddress(QString hostName, unsigned long& address){
         return false;
 
     h_addr.s_addr = *((unsigned long *) host -> h_addr_list[0]);
+    address = *((unsigned long *) host -> h_addr_list[0]);
     return true;
-}
-
-bool LowNetwork::getSocket(int& socketHandle, unsigned long address, int port){
-    struct sockaddr_in peerDescription;
-    socketHandle = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(socketHandle < 0)
-        return false;
-    peerDescription.sin_family = AF_INET;
-    peerDescription.sin_addr.s_addr = address;
-    peerDescription.sin_port = htons(port);
-
-    return connect(socketHandle, (struct sockaddr *) &peerDescription, sizeof(peerDescription)) >= 0;
 }
--- ChatClient/lownetwork.h
+++ ChatClient/lownetwork.h
@@ -11,8 +11,13 @@
 class LowNetwork
 {
 public:
-    bool getHostAddress(QString hostName, unsigned long& hostInt);
-    bool getSocket(int& socketHandle, unsigned long address, int port);
+    int server(const QString port);
+    int client(const QString host, const QString port);
+private:
+    unsigned long mAdress;
+    short mPort;
+    int mSocketHandle;
+    bool getHostAddress(const QString hostName, unsigned long& hostInt);
 };
 
 #endif // LOWNETWORK_H
